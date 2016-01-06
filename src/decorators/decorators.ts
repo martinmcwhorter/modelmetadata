b5b13b42e433593d91ff38d2825ca093a56b64b9
Martin McWhorter <martin@mcwhorter.org>
2016-01-06 12:07:21 +0000

    update

--- src/decorators/decorators.ts
+++ src/decorators/decorators.ts
@@ -3,26 +3,26 @@ import {decoratorFactory} from './decoratorFactory';
 
 export let Label: (label: string) => any = decoratorFactory(c.LABEL);
 
-export let Required: (message?: string) => any = decoratorFactory(c.REQUIRED, true);
+export let Required: (errorMessage?: string) => any = decoratorFactory(c.REQUIRED, true);
 
-export let Pattern: (pattern: RegExp, message?: string) => any = decoratorFactory(c.PATTERN);
+export let Pattern: (pattern: RegExp, errorMessage?: string) => any = decoratorFactory(c.PATTERN);
 
-export let Max: (length: number, message?: string) => any = decoratorFactory(c.MAX);
+export let Max: (length: number, errorMessage?: string) => any = decoratorFactory(c.MAX);
 
-export let MaxLength: (length: number, message?: string) => any = decoratorFactory(c.MAX_LENGTH);
+export let MaxLength: (length: number, errorMessage?: string) => any = decoratorFactory(c.MAX_LENGTH);
 
-export let Min: (length: number, message?: string) => any = decoratorFactory(c.MIN);
+export let Min: (length: number, errorMessage?: string) => any = decoratorFactory(c.MIN);
 
-export let MinLength: (length: number, message?: string) => any = decoratorFactory(c.MIN_LENGTH);
+export let MinLength: (length: number, errorMessage?: string) => any = decoratorFactory(c.MIN_LENGTH);
 
-export let Integer: (message?: string) => any = decoratorFactory(c.INTEGER, true);
+export let Integer: (errorMessage?: string) => any = decoratorFactory(c.INTEGER, true);
 
-export let Default: (value: any, message?: string) => any = decoratorFactory(c.DEFAULT);
+export let Default: (value: any, errorMessage?: string) => any = decoratorFactory(c.DEFAULT);
 
-export let Enum: (values: any[], message?: string) => any = decoratorFactory(c.ENUM);
+export let Enum: (values: any[], errorMessage?: string) => any = decoratorFactory(c.ENUM);
 
-export let Description: (description: string, message?: string) => any = decoratorFactory(c.DESCRIPTION);
+export let Description: (description: string, errorMessage?: string) => any = decoratorFactory(c.DESCRIPTION);
 
-export let Format: (format: string, message?: string) => any = decoratorFactory(c.FORMAT);
+export let Format: (format: string) => any = decoratorFactory(c.FORMAT);
 
 
